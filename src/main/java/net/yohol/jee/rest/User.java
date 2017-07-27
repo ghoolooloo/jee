d41c245c8eede6783f8c6e5296a9f5f846056ff4
zhouq <zhouq@mayocase.com>
2017-07-27 16:12:52 +0800

    试用resourceAccess

--- src/main/java/net/yohol/jee/rest/User.java
+++ src/main/java/net/yohol/jee/rest/User.java
@@ -8,6 +8,7 @@ public class User {
   private String email;
   private String subject;
   private Set<String> roles;
+  private Set<String> roles2;
 
   public String getUsername() {
     return username;
@@ -42,5 +43,12 @@ public class User {
   }
   public void setRoles(Set<String> roles) {
     this.roles = roles;
+  }
+
+  public Set<String> getRoles2() {
+    return roles2;
+  }
+  public void setRoles2(Set<String> roles2) {
+    this.roles2 = roles2;
   }
 }
